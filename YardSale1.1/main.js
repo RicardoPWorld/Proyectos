8bcf708f3fe623a26647ad1cfc75d686a9114cf9
Ricaro Donoso <rndm2002@gmail.com>
2022-09-12 16:59:32 -0300

    no se que tenia pendiente, pero agregamos el google clon que estamos haciendo y empezando 12/09/2022

--- YardSale1.1/main.js
+++ YardSale1.1/main.js
@@ -1,20 +1,31 @@
-const menuCart = document.querySelector('.nav-icon-cart');
+const menuCart = document.querySelector('.nav-icon-cart-img');
 const menuAccount = document.querySelector('.nav-menu-usuario');
 const menuEmail = document.querySelector('.nav-email');
-const menuArrow = document.querySelector('.nav-icon-arrow')
+const menuArrow = document.querySelector('.nav-icon-arrow');
+const asideProduct = document.querySelector('.cart-product');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuArrow.addEventListener('click', toggleDesktopMenu);
+menuCart.addEventListener('click', toggleAside);
 
 function toggleDesktopMenu()
 {
-    const navMenu = menuAccount.classList.contains('inactive')
-    if(navMenu)
+    const navMenu = asideProduct.classList.contains('inactive')
+    if(!navMenu)
     {
-        menuAccount.classList.remove('inactive')
+        asideProduct.classList.add('inactive')
     }
-    else
+    
+    menuAccount.classList.toggle('inactive');
+    
+}
+function toggleAside()
+{
+    const navAside = menuAccount.classList.contains('inactive')
+    if(!navAside)
     {
-        menuAccount.classList.add('inactive')
+        menuAccount.classList.add('inactive');
     }
+    asideProduct.classList.toggle('inactive');
+    
 }
